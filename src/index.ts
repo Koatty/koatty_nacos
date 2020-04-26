63647b7ea2b39a72bdd77ca99a4cc8d5e753dc55
richen <richenlin@gmail.com>
2020-04-27 00:26:21 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+/**
+ * @ author: richen
+ * @ copyright: Copyright (c) - <richenlin(at)gmail.com>
+ * @ license: MIT
+ * @ version: 2020-04-26 23:43:26
+ */
+
+import { Koatty, Middleware } from "../Koatty";
+
+@Middleware()
+export class Payload {
+    run(options: any, app: Koatty) {
+
+    }
+}
